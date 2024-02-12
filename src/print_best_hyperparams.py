b41e3f6dc04e4443715f78766c1a03f82dd64d4f
thecml <chr1000@gmail.com>
2024-02-12 15:44:13 +0100

    Added print hyperparameters

---
+++ src/print_best_hyperparams.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import config as cfg
+from glob import glob
+import numpy as np
+import re
+
+if __name__ == "__main__":
+    path = cfg.RESULTS_PATH
+    results = pd.read_csv(f'{path}/model_results.csv', index_col=0)
+    conditions = ["C3æ"]
+    censoring = [0.25]
+    model_names = ["CoxPH", "RSF", "DeepSurv", "DSM", "BNNmcd"]
+    for cond in conditions:
+        for index, model_name in enumerate(model_names):
+            text = ""            
+            text += f"& {model_name} & "
+            for cens in censoring:
+                best_params =  results.loc[(results['Condition'] == cond) & (results['CensoringLevel'] == cens) & (results['ModelName'] == model_name)]['BestParams']
+                text += str(best_params.values)
+            print(text)
+        print()
